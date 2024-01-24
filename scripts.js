7012b6d26cbbfb384e7de7f723626272782da4fd
aferoliv <41442326+aferoliv@users.noreply.github.com>
2024-01-24 16:32:02 -0300

    TextBox
    TextBox1 com cabeçalho: 
            - falta apagar o anterior;
            - colocar botão para resetar os dados sem mexer influenciar o arduino;
    TextBox2 com cabeçalho e valores
             - apresentar o volume acumulado;
    Gráfico:
             - criar novas series;
             - criar matriz com os valores coletados como número;
    Salvar dados: 
             - salvar os dados;

--- scripts.js
+++ scripts.js
@@ -1,13 +1,18 @@
 let titr = {
   data_array: [0],
+  data_grafico:[0],
   curva: [0],
   tempo: [0],
 };
+let pontos = 0;
 let data_linha = [0, 0, 0];
 let canal = [0, 0, 0];
+let volumetotal = [0];
 var transito = 1;
+let volume = 0;
 const captura = document.getElementById("captura_dados");
 const sair_leitura = document.getElementById("sair");
+
 document.getElementById("buttonVolume").addEventListener("click", aliquota());
 
 document.getElementById("connectButton").addEventListener("click", async () => {
@@ -34,30 +39,38 @@ document.getElementById("connectButton").addEventListener("click", async () => {
       // Converte of format Uint8Array para string para adicionar ao elemento HTML.
       const textDecoder = new TextDecoder();
       const text = textDecoder.decode(value);
+      //console.log(text, textDecoder);
+
       const linha = text.split("b'");
       let data_linha = linha;
-
-      canal = parseFloat(data_linha[1]);
-      //canal = parseFloat(data_linha[0]);
-      //let canal[1] = parseFloat(data_linha[1]);
-      //let canal[2] = parseFloat(data_linha[2]);
-
-      document.getElementById("dataLog").value += data_linha;
+      canal = parseFloat(data_linha[0]);      //
+      // array canal: converte vetor data_linha em float
+      //
+      /*data_linha.forEach((element) => {
+        canal = parseFloat(data_linha[element]);
+        console.log(typeof canal, data_linha.slice(","), typeof linha);
+      });
+      */
+      if (text.includes("niciando")) {
+        document.getElementById("dataLog").value += "\t \t dados \n";
+        document.getElementById("dataTitr").value += "# \t volume \t dados \n";
+      } else {
+        document.getElementById("dataLog").value += data_linha;
+      }
       // WebTransportBidirectionalStream.envio;
-      console.log(data_linha.length, data_linha, "canal", canal);
+      //  
+      //atualiza o gráfico principal
+      //
       if (data_linha.length == 3) {
         temp++;
-        titr.tempo.push(temp);
-        titr.data_array.push(canal);
-        cinGrafico.data.labels = titr.tempo;
-        //
-        console.log("dentro", titr.data_array);
-        //
+        titr.tempo.push(temp);                  //acrescenta a contagem de elementos
+        titr.data_array.push(data_linha);       // transfere a string da Serial para array   
+        titr.data_grafico.push(canal);          // acrescenta a primeira coluna no vetor data_grafico 
+        cinGrafico.data.labels = titr.tempo;    // eixo X
         cinGrafico.data.datasets[0].backgroundColor = "rgba(25, 9, 132, 1)";
-        cinGrafico.data.datasets[0].data = titr.data_array;
-        //cinGrafico.data.datasets[1].data = titr.data_array[1];
-        //cinGrafico.data.datasets[2].data = titr.data_array;
-        cinGrafico.update();
+        cinGrafico.data.datasets[0].data = canal[0];  // eixo Y serie 1
+        
+        cinGrafico.update();          //atualiza o gráfico
       }
 
       //if (data_linha.length == 3) {}
@@ -75,15 +88,25 @@ document.getElementById("connectButton").addEventListener("click", async () => {
 
 captura.addEventListener("click", function () {
   titr.curva.push(titr.data_linha);
-  const file = document.getElementById("input").value;
-  document.getElementById("dataTitr").value += data_linha;
-  console.log("saveFile", titr.curva);
-  console.log(file);
+  pontos++;
+  volume += parseFloat(document.getElementById("inputVolume").value);
+  if (volume == 0) {
+    volume = 100;
+  }
+  volumetotal.push(volume);
+  const nome = document.getElementById("input").value;
+  document.getElementById("dataTitr").value += pontos;
+  document.getElementById("dataTitr").value += "\t";
+  document.getElementById("dataTitr").value += volume;
+  document.getElementById("dataTitr").value += "\t";
+  document.getElementById("dataTitr").value +=
+    titr.data_array[titr.data_array.length - 1];
+  document.getElementById("dataTitr").value += "\n";
+
+  console.log("saveFile", nome);
 });
-sair_leitura.addEventListener("click", function () {
-  transito = 0;
-  console.log("sair", transito);
-});
+volume = document.getElementById("buttonVolume").value;
+
 function aliquota() {
   console.log("dentro aliquota");
   volume = document.getElementById("inputVolume").value;
